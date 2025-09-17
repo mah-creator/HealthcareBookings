2c1c2626a2e3d870075f1495f2e8aaafe084401c
mahmoud <drivemah.tah@gmail.com>
2025-09-17 14:40:30 +0300

    bug fix

--- HealthcareBookings.Application/Doctors/Utils.cs
+++ HealthcareBookings.Application/Doctors/Utils.cs
@@ -10,15 +10,15 @@ public static class Utils
 		return new DoctorDto
 		{
 			Id = d.DoctorUID,
-			Name = d.Account.Profile.Name,
+			Name = d.Account?.Profile?.Name,
 			IsFavorite = patient.PatientProperties?.FavoriteDoctors?.Find(fd => fd.DoctorID == d.DoctorUID) is not null,
-			ClinicName = d.Clinic.ClinicName,
-			ClinicLocation = d.Clinic.Location.ToString(),
+			ClinicName = d.Clinic?.ClinicName,
+			ClinicLocation = d.Clinic?.Location?.ToString(),
 			Rating = d.Rating,
-			Reviews = d.Appointments.Count(a => a.Review != null),
+			Reviews = d.Appointments?.Count(a => a.Review != null)?? 0,
 			Experience = d.ExperienceYears,
 			Bio = d.Bio,
-			PatientCount = d.Appointments.DistinctBy(a => a.PatientID).Count()
+			PatientCount = d.Appointments?.DistinctBy(a => a.PatientID).Count() ?? 0
 		};
 	}
 }
