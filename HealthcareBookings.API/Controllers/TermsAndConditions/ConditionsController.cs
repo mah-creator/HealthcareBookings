cf427532c10d5d51be134e5b13afdf911ed33e4e
mahmoud <drivemah.tah@gmail.com>
2025-10-11 18:56:25 +0300

    terms and conditions page

---
+++ HealthcareBookings.API/Controllers/TermsAndConditions/ConditionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthcareBookings.API.Controllers.TermsAndConditions;
+
+[ApiController]
+public class HtmlController : ControllerBase
+{
+	private readonly IWebHostEnvironment _webHostEnvironment;
+
+	public HtmlController(IWebHostEnvironment webHostEnvironment)
+	{
+		_webHostEnvironment = webHostEnvironment;
+	}
+
+	[Route("/terms-and-conditions")]
+	public ContentResult GetStaticHtmlPage()
+	{
+		var filePath = Path.Combine(_webHostEnvironment.WebRootPath, "terms-and-conditions.html");
+		if (!System.IO.File.Exists(filePath))
+		{
+			return Content("Error: HTML file not found.", "text/plain");
+		}
+
+		var htmlContent = System.IO.File.ReadAllText(filePath);
+		return Content(htmlContent, "text/html");
+	}
+}
